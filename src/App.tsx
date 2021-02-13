cea87da8dc2c7c469b5328752c1982aa81d7eb15
Roy van de Water <roy.vwater@gmail.com>
2021-02-12 21:46:57 -0700

    More

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const useData = () => {
+  const [data, setData] = React.useState()
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('/data.json')
+      setData(await res.json())
+    }
+
+    fetchData();
+  }, [])
+
+  return data
+}
+
+
 function App() {
+  const data = useData()
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
       </header>
     </div>
   );
